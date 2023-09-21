b3974c90a9118290b19cf53fe21bd6ca0fd61ef6
Agile Soft <agilesoftsystem@gmail.com>
2023-09-21 13:46:44 -0300

    section portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,3 +65,42 @@ back_to_top.addEventListener("click", function (e) {
         behavior: "smooth",
     });
 });
+
+
+//PORTIFÓLIO
+
+let filter_btns = document.querySelectorAll('.filter-btn1');
+let projects = document.querySelectorAll('.filter-project-item1');
+
+filter_btns.forEach(filter_btn => {
+  filter_btn.addEventListener('click', () => {
+    filter_btns.forEach(filter_btn => {
+      filter_btn.classList.remove('active');
+    });
+    filter_btn.classList.add('active');
+    let selected = filter_btn.getAttribute('data-project');
+    projects.forEach((project, index) => {
+      let project_length = projects.length;
+      if (
+        project.getAttribute('data-project') === selected ||
+        selected === 'todos'
+      ) {
+        if (project.style.transition) {
+          project.style.transition = '0.5s';
+        } else {
+          project.style.transition = `width 0.5s ease ${
+            index / project_length + 0.6
+          }s,height 0.5s ease ${index / project_length + 0.6}s`;
+        }
+        project.classList.add('show');
+        project.classList.remove('hide');
+      } else {
+        project.style.transition = `width 0.5s ease ${
+          index / project_length + 0.6
+        }s,height 0.5s ease ${index / project_length + 0.6}s`;
+        project.classList.remove('show');
+        project.classList.add('hide');
+      }
+    });
+  });
+});
