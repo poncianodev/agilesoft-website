a84079f265b2a6f22883d33747c73337d6f6c402
poncianodev <lucasbponciano@hotmail.com>
2023-09-23 16:38:12 -0300

    alterações na sessão de portfólio

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,33 +20,32 @@ window.onload = function () {
 
 // SEÇÃO PROJETOS
 
-let filter_btn = document.querySelectorAll('.filter-btn');
-let tab_items = document.querySelectorAll('.tab-item');
+let filter_btn = document.querySelectorAll(".filter-btn");
+let tab_items = document.querySelectorAll(".tab-item");
 
 for (let i = 0; i < filter_btn.length; i++) {
-  filter_btn[i].addEventListener('click', function () {
-    for (let j = 0; j < filter_btn.length; j++) {
-      filter_btn[j].classList.remove('active');
-    }
-    let select_tab = filter_btn[i].getAttribute('data-tab');
-    filter_btn[i].classList.add('active');
-    for (let t = 0; t < tab_items.length; t++) {
-      document.querySelector('.tab-filter-item-container').style.height =
-        tab_items[t].scrollHeight + 'px';
-      if (tab_items[t].classList.contains(select_tab)) {
-        tab_items[t].classList.add('select_tab');
-      } else {
-        tab_items[t].classList.remove('select_tab');
-      }
-    }
-  });
+    filter_btn[i].addEventListener("click", function () {
+        for (let j = 0; j < filter_btn.length; j++) {
+            filter_btn[j].classList.remove("active");
+        }
+        let select_tab = filter_btn[i].getAttribute("data-tab");
+        filter_btn[i].classList.add("active");
+        for (let t = 0; t < tab_items.length; t++) {
+            document.querySelector(".tab-filter-item-container").style.height =
+                tab_items[t].scrollHeight + "px";
+            if (tab_items[t].classList.contains(select_tab)) {
+                tab_items[t].classList.add("select_tab");
+            } else {
+                tab_items[t].classList.remove("select_tab");
+            }
+        }
+    });
 }
 
 for (let th = 0; th < tab_items.length; th++) {
-  document.querySelector('.tab-filter-item-container').style.height =
-    tab_items[th].scrollHeight + 'px';
+    document.querySelector(".tab-filter-item-container").style.height =
+        tab_items[th].scrollHeight + "px";
 }
-
 
 // EFEITO VOLTAR AO TOPO
 
@@ -66,41 +65,27 @@ back_to_top.addEventListener("click", function (e) {
     });
 });
 
-
 //PORTIFÓLIO
 
-let filter_btns = document.querySelectorAll('.filter-btn1');
-let projects = document.querySelectorAll('.filter-project-item1');
+const buttonSection = document.querySelectorAll(".buttons-section button");
+const cardSection = document.querySelectorAll(".cards-section .card");
 
-filter_btns.forEach(filter_btn => {
-  filter_btn.addEventListener('click', () => {
-    filter_btns.forEach(filter_btn => {
-      filter_btn.classList.remove('active');
-    });
-    filter_btn.classList.add('active');
-    let selected = filter_btn.getAttribute('data-project');
-    projects.forEach((project, index) => {
-      let project_length = projects.length;
-      if (
-        project.getAttribute('data-project') === selected ||
-        selected === 'todos'
-      ) {
-        if (project.style.transition) {
-          project.style.transition = '0.5s';
-        } else {
-          project.style.transition = `width 0.5s ease ${
-            index / project_length + 0.6
-          }s,height 0.5s ease ${index / project_length + 0.6}s`;
+const filtercards = e => {
+    document
+        .querySelector(".active-portfolio")
+        .classList.remove("active-portfolio");
+    e.target.classList.add("active-portfolio");
+
+    cardSection.forEach(card => {
+        card.classList.add("hide");
+
+        if (
+            card.dataset.name === e.target.dataset.name ||
+            e.target.dataset.name === "all"
+        ) {
+            card.classList.remove("hide");
         }
-        project.classList.add('show');
-        project.classList.remove('hide');
-      } else {
-        project.style.transition = `width 0.5s ease ${
-          index / project_length + 0.6
-        }s,height 0.5s ease ${index / project_length + 0.6}s`;
-        project.classList.remove('show');
-        project.classList.add('hide');
-      }
     });
-  });
-});
+};
+
+buttonSection.forEach(button => button.addEventListener("click", filtercards));
