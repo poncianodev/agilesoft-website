3f1032a7e219c214234494a7b00117fb14d2c0d0
Thales <agilesoftsystem@gmail.com>
2023-10-09 15:29:19 -0300

    alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,45 @@
 // EFEITO ROLAGEM NAV BAR
 
 window.onload = function () {
-    window.addEventListener("scroll", function (e) {
-        if (window.pageYOffset > 100) {
-            document.querySelector("header").classList.add("scroll");
-        } else {
-            document.querySelector("header").classList.remove("scroll");
-        }
-    });
+  window.addEventListener('scroll', function (e) {
+    if (window.pageYOffset > 100) {
+      document.querySelector('header').classList.add('scroll');
+    } else {
+      document.querySelector('header').classList.remove('scroll');
+    }
+  });
 
-    const menu_btn = document.querySelector(".hamburger");
-    const mobile_menu = document.querySelector(".mobile-nav");
+  const menu_btn = document.querySelector('.hamburger');
+  const mobile_menu = document.querySelector('.mobile-nav');
 
-    menu_btn.addEventListener("click", function () {
-        menu_btn.classList.toggle("is-active");
-        mobile_menu.classList.toggle("is-active");
-    });
+  menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+  });
 };
 
 // SEÇÃO PROJETOS
 
-let filter_btn = document.querySelectorAll(".filter-btn");
-let tab_items = document.querySelectorAll(".tab-item");
+let filter_btn = document.querySelectorAll('.filter-btn');
+let tab_items = document.querySelectorAll('.tab-item');
 
 for (let i = 0; i < filter_btn.length; i++) {
-    filter_btn[i].addEventListener("click", function () {
-        for (let j = 0; j < filter_btn.length; j++) {
-            filter_btn[j].classList.remove("active");
-        }
-        let select_tab = filter_btn[i].getAttribute("data-tab");
-        filter_btn[i].classList.add("active");
-        for (let t = 0; t < tab_items.length; t++) {
-            // document.querySelector(".tab-filter-item-container").style.height =
-            //     tab_items[t].scrollHeight + "px";
-            if (tab_items[t].classList.contains(select_tab)) {
-                tab_items[t].classList.add("select_tab");
-            } else {
-                tab_items[t].classList.remove("select_tab");
-            }
-        }
-    });
+  filter_btn[i].addEventListener('click', function () {
+    for (let j = 0; j < filter_btn.length; j++) {
+      filter_btn[j].classList.remove('active');
+    }
+    let select_tab = filter_btn[i].getAttribute('data-tab');
+    filter_btn[i].classList.add('active');
+    for (let t = 0; t < tab_items.length; t++) {
+      // document.querySelector(".tab-filter-item-container").style.height =
+      //     tab_items[t].scrollHeight + "px";
+      if (tab_items[t].classList.contains(select_tab)) {
+        tab_items[t].classList.add('select_tab');
+      } else {
+        tab_items[t].classList.remove('select_tab');
+      }
+    }
+  });
 }
 
 // for (let th = 0; th < tab_items.length; th++) {
@@ -49,48 +49,54 @@ for (let i = 0; i < filter_btn.length; i++) {
 
 // EFEITO VOLTAR AO TOPO
 
-let back_to_top = document.querySelector(".back_to_top");
-window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 100) {
-        back_to_top.classList.add("show");
-    } else {
-        back_to_top.classList.remove("show");
-    }
+let back_to_top = document.querySelector('.back_to_top');
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 100) {
+    back_to_top.classList.add('show');
+  } else {
+    back_to_top.classList.remove('show');
+  }
 });
-back_to_top.addEventListener("click", function (e) {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-    });
+back_to_top.addEventListener('click', function (e) {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
 });
 
 //PORTIFÓLIO
 
-const buttonSection = document.querySelectorAll(".buttons-section button");
-const cardSection = document.querySelectorAll(".cards-section .card");
+const buttonSection = document.querySelectorAll('.buttons-section button');
+const cardSection = document.querySelectorAll('.cards-section .card');
 
 const filtercards = e => {
-    document
-        .querySelector(".active-portfolio")
-        .classList.remove("active-portfolio");
-    e.target.classList.add("active-portfolio");
+  document
+    .querySelector('.active-portfolio')
+    .classList.remove('active-portfolio');
+  e.target.classList.add('active-portfolio');
 
-    cardSection.forEach(card => {
-        card.classList.add("hide");
+  cardSection.forEach(card => {
+    card.classList.add('hide');
 
-        if (
-            card.dataset.name === e.target.dataset.name ||
-            e.target.dataset.name === "all"
-        ) {
-            card.classList.remove("hide");
-        }
-    });
+    if (
+      card.dataset.name === e.target.dataset.name ||
+      e.target.dataset.name === 'all'
+    ) {
+      card.classList.remove('hide');
+    }
+  });
 };
 
-buttonSection.forEach(button => button.addEventListener("click", filtercards));
+buttonSection.forEach(button => button.addEventListener('click', filtercards));
 
 //INPUT-TEL
-document.getElementById("telefone").addEventListener("input", function () {
-    this.value = this.value.replace(/\D/g, "");
+document.getElementById('telefone').addEventListener('input', function () {
+  this.value = this.value.replace(/\D/g, '');
 });
+
+function alertar() {
+  setTimeout(function () {
+    alert('Sua mensagem foi enviada!');
+  }, 2000);
+}
